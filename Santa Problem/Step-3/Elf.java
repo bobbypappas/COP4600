38b46ce5a71ff40111ce5b70a0ba27c38fbc8924
Bobby Pappas <55039044+bobbypappas@users.noreply.github.com>
2021-07-22 10:21:33 -0400

    Add files via upload

---
+++ Santa Problem/Step-3/Elf.java
@@ -0,0 +1,86 @@
+import java.util.Random;
+
+public class Elf implements Runnable {
+	int elfCounter = 0;
+
+	enum ElfState {
+		WORKING, TROUBLE, AT_SANTAS_DOOR
+	};
+
+	private ElfState state;
+	/**
+	 * The number associated with the Elf
+	 */
+	private int number;
+	private Random rand = new Random();
+	private SantaScenario scenario;
+	private boolean bool = true;
+
+	public Elf(int number, SantaScenario scenario) {
+		this.number = number;
+		this.scenario = scenario;
+		this.state = ElfState.WORKING;
+	}
+
+	public ElfState getState() {
+		return state;
+	}
+
+	/**
+	 * Santa might call this function to fix the trouble
+	 * @param state
+	 */
+	public void setState(ElfState state) {
+		this.state = state;
+	}
+
+
+	@Override
+	public void run() {
+		while (bool) {
+      // wait a day
+  		try {
+  			Thread.sleep(100);
+  		} catch (InterruptedException e) {
+  			// TODO Auto-generated catch block
+  			e.printStackTrace();
+  		}
+			switch (state) {
+			case WORKING: {
+				// at each day, there is a 1% chance that an elf runs into
+				// trouble.
+				if (rand.nextDouble() < 0.01) {
+					state = ElfState.TROUBLE;
+					elfCounter++;
+					System.out.println(elfCounter);
+				}
+				break;
+			}
+			case TROUBLE:
+				// FIXME: if possible, move to Santa's door
+				state = ElfState.AT_SANTAS_DOOR;
+				break;
+			case AT_SANTAS_DOOR:
+				// FIXME: if feasible, wake up Santa
+				if(elfCounter == 3)
+				{
+					scenario.santa.elvesInTrouble();
+					elfCounter = 0;
+				}
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Report about my state
+	 */
+	public void report() {
+		System.out.println("Elf " + number + " : " + state);
+	}
+
+	public void endProcess()
+	{
+		bool = false;
+	}
+}
